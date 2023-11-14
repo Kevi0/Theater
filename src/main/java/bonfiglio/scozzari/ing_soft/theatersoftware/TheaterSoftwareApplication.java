01edacc2638d07a9920a7d1b55217e4a352e781b
mattia757 <mattiabonfiglio1@gmail.com>
2023-11-14 23:01:55 +0100

    First Commit

---
+++ src/main/java/bonfiglio/scozzari/ing_soft/theatersoftware/TheaterSoftwareApplication.java
@@ -0,0 +1,13 @@
+package bonfiglio.scozzari.ing_soft.theatersoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheaterSoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bonfiglio/scozzari/ing_soft/theatersoftware/TheaterSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package bonfiglio.scozzari.ing_soft.theatersoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
