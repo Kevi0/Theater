771a83ed36269afdd40f8bf129e5523a8709a4f9
Kevi0 <kscozzari03@gmail.com>
2024-02-15 10:26:12 +0100

    Response Message

---
+++ src/main/java/bonfiglio/scozzari/ing_soft/theatersoftware/response/ResponseMessage.java
@@ -0,0 +1,14 @@
+package bonfiglio.scozzari.ing_soft.theatersoftware.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseMessage {
+
+    private String message;
+
+}
