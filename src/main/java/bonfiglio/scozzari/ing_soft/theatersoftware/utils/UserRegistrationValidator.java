8b37e145c5b56c8630a63e06972a55c856d94873
Kevi0 <kscozzari03@gmail.com>
2024-02-24 11:49:10 +0100

    UserRegistrationValidator

--- src/main/java/bonfiglio/scozzari/ing_soft/theatersoftware/utils/UserRegistrationValidator.java
+++ src/main/java/bonfiglio/scozzari/ing_soft/theatersoftware/utils/UserRegistrationValidator.java
@@ -14,9 +14,7 @@ public class UserRegistrationValidator {
 
         validateName(user.getName());
         validateSurname(user.getSurname());
-        validateTaxCode(user.getTaxCode());
         validateEmail(user.getEmail());
-        validateUsername(user.getName(), user.getSurname(), user.getUsername());
         validatePassword(user.getName(), user.getSurname(), user.getPassword());
 
     }
@@ -27,20 +25,8 @@ public class UserRegistrationValidator {
             throw new InvalidDataException("Errore durante la registrazione: il campo 'name' non può essere vuoto!");
         }
 
-        if (!name.matches(RegexPatterns.LENGTH_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'name' deve avere una lunghezza compresa tra 3 e 40 caratteri!");
-        }
-
-        if (!name.matches(RegexPatterns.FORMAT_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'name' deve iniziare con una lettera maiuscola seguita da lettere minuscole!");
-        }
-
-        if (name.matches(RegexPatterns.NOT_NUMBERS_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'name' non può contenere numeri!");
-        }
-
-        if (name.matches(RegexPatterns.NOT_SPECIAL_CHARACTERS_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'name' non può contenere caratteri speciali!");
+        if (!name.matches(RegexPatterns.NAME_PATTERN)) {
+            throw new InvalidDataException("Errore durante la registrazione: il campo 'name' deve avere una lunghezza compresa tra 3 e 30 caratteri, il primo carattere deve essere MAIUSCOLO e non può contenere numeri e/o caratteri speciali!");
         }
 
     }
@@ -51,20 +37,8 @@ public class UserRegistrationValidator {
             throw new InvalidDataException("Errore durante la registrazione: il campo 'surname' non può essere vuoto!");
         }
 
-        if (!surname.matches(RegexPatterns.LENGTH_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'surname' deve avere una lunghezza compresa tra 3 e 50 caratteri!");
-        }
-
-        if (!surname.matches(RegexPatterns.FORMAT_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'surname' deve iniziare con una lettera maiuscola seguita da lettere minuscole!");
-        }
-
-        if (surname.matches(RegexPatterns.NOT_NUMBERS_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'surname' non può contenere numeri!");
-        }
-
-        if (surname.matches(RegexPatterns.NOT_SPECIAL_CHARACTERS_NAME_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'surname' non può contenere caratteri speciali!");
+        if (!surname.matches(RegexPatterns.NAME_PATTERN)) {
+            throw new InvalidDataException("Errore durante la registrazione: il campo 'surname' deve avere una lunghezza compresa tra 3 e 30 caratteri, il primo carattere deve essere MAIUSCOLO e non può contenere numeri e/o caratteri speciali!");
         }
 
     }
@@ -76,30 +50,6 @@ public class UserRegistrationValidator {
         if (!email.matches(RegexPatterns.EMAIL_PATTERN)) {
             throw new InvalidDataException("Errore durante la registrazione: il campo 'email' non è nel formato corretto!");
         }
-    }
-
-    private void validateTaxCode(String taxCode) throws InvalidDataException {
-
-        if (taxCode == null || taxCode.trim().isEmpty()) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'codiceFiscale' non può essere vuoto!");
-        }
-
-        if (!taxCode.matches(RegexPatterns.TAX_CODE_PATTERN)) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'codiceFiscale' non è valido!");
-        }
-
-    }
-
-    private void validateUsername(String name, String surname, String username) throws InvalidDataException {
-
-        if (username == null || username.trim().isEmpty()) {
-            throw new InvalidDataException("Errore durante la registrazione: il campo 'username' non può essere vuoto!");
-        }
-
-        if (!(username.equals(name.trim().toLowerCase() + "." + surname.trim().toLowerCase()))) {
-            throw new InvalidDataException("Errore durante la registrazione: l'username non segue la notazione 'nome.cognome'!");
-        }
-
     }
 
     private void validatePassword(String name, String surname, String password) throws InvalidDataException {
@@ -115,7 +65,6 @@ public class UserRegistrationValidator {
         if (password.matches(RegexPatterns.COMMON_PASSWORDS_PATTERN + Pattern.quote(name) + "|" + Pattern.quote(surname) + ").*")) {
             throw new InvalidDataException("Errore durante la registrazione: la password non può contenere sequenze comuni o informazioni personali!");
         }
-
 
     }
 
